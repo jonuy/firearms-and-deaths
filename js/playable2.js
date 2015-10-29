074f279490365c0ab24504ab5a7bb63bc9bc7638
jon uy <jonxuy@gmail.com>
2015-10-29 01:58:43 -0400

    Draw the 'start simulation' button

--- js/playable2.js
+++ js/playable2.js
@@ -16,8 +16,12 @@ playable2 = (function() {
   var CANVAS_GRID_SIZE = 12;
 
   // Colors
-  var COLOR_GRID = '#cccccc';
   var COLOR_CROSSHAIRS = '#cc3333';
+  var COLOR_GRID = '#cccccc';
+  var COLOR_BUTTON_HOVER = '#cccccc';
+  var COLOR_BUTTON_OFF = '#333333';
+  var COLOR_BUTTON_ON = '#777777';
+  var COLOR_BUTTON_TEXT = '#ffffff';
 
   // Canvas and context
   var canvas;
@@ -29,9 +33,15 @@ playable2 = (function() {
   // Boolean. True if we should draw debug stuff.
   var showDebug = false;
 
+  // Boolean. True if the simulation is running.
+  var isRunning = false;
+
   // Current mouse pointer positions
   var mouseX;
   var mouseY;
+
+  // Event queue
+  var eventQueue = [];
 
   /**
    * Setup initial view of the canvas
@@ -54,7 +64,7 @@ playable2 = (function() {
    * @todo Comment me
    */
   function onClick(event) {
-
+    eventQueue.push(event);
   }
 
   /**
@@ -121,6 +131,8 @@ playable2 = (function() {
     ctx.stroke();
 
     // Draw positions
+    ctx.font = '12px Helvetica';
+    ctx.textAlign = 'left';
     ctx.fillText('x: ' + mouseX, CANVAS_WIDTH - 40, 12);
     ctx.fillText('y: ' + mouseY, CANVAS_WIDTH - 40, 24);
   }
@@ -136,7 +148,75 @@ playable2 = (function() {
       debugDrawPointer();
     }
 
+    drawStartButton();
+
+    // If nothing's consumed an event in the queue, then clear it all.
+    // um... is this the right thing to do? Could we run into some weird
+    // race condition where a click happens but nothing in this loop
+    // actually gets a chance to see it?
+    eventQueue = [];
+
     window.requestAnimationFrame(draw);
+  }
+
+  /**
+   * Draw the "start simulation" button
+   */
+  function drawStartButton() {
+    var i;
+    var text;
+    var isClicked = false;;
+    var boxWidth = 200;
+    var boxHeight = 36;
+    var boxMargin = 12;
+
+    var xPosBox = (CANVAS_WIDTH - boxWidth) / 2;
+    var yPosBox = boxMargin;
+
+    function inButtonBounds(x, y) {
+      return mouseX >= xPosBox && mouseX <= xPosBox + boxWidth &&
+          mouseY >= yPosBox && mouseY <= yPosBox + boxHeight
+    }
+
+    // Check event queue for any clicks
+    i = eventQueue.length;
+    while (i--) {
+      if (eventQueue[i].type == 'click') {
+        if (inButtonBounds(eventQueue[i].layerX, eventQueue[i].layerY)) {
+          eventQueue.splice(i, 1);
+          isClicked = true;
+        }
+      }
+    }
+
+    // If there was a click, switch the isRunning flag
+    if (isClicked) {
+      isRunning = !isRunning;
+    }
+
+    if (isRunning) {
+      ctx.fillStyle = COLOR_BUTTON_ON;
+      text = 'STOP SIMULATION';
+    }
+    else {
+      ctx.fillStyle = COLOR_BUTTON_OFF;
+      text = 'START SIMULATION';
+    }
+
+    // Is mouse currently hovering over
+    if (inButtonBounds(mouseX, mouseY)) {
+      ctx.fillStyle = COLOR_BUTTON_HOVER;
+    }
+
+    // Draw the button
+    ctx.fillRect(xPosBox, yPosBox, boxWidth, boxHeight);
+
+    // Draw text
+    ctx.font = '16px Helvetica';
+    ctx.fillStyle = COLOR_BUTTON_TEXT;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(text, CANVAS_WIDTH / 2, boxMargin + (boxHeight / 2));
   }
 
   return {
