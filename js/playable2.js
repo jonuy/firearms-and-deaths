3bc897d34d875b2921730a88be1a5e2430bfc486
jon uy <jonxuy@gmail.com>
2015-10-29 15:40:08 -0400

    Adds drawing of sections boxes and labels for canvas2

--- js/playable2.js
+++ js/playable2.js
@@ -16,6 +16,12 @@ playable2 = (function() {
   var CANVAS_GRID_SIZE = 12;
 
   // Colors
+  var COLOR_BOUNDARIES = '#000000';
+  var COLOR_GUN_LOCK = '#92bfb4'; // @todo may want to revisit this color
+  var COLOR_ATTEMPT = '#ec9e56'; // @todo may want to revist this color
+  var COLOR_SAVED = '#00ff00'; // @todo change me
+  var COLOR_UNSUCCESSFUL = '#ffff00'; // @todo change me
+  var COLOR_FATAL = '#ff0000'; // @todo change me
   var COLOR_CROSSHAIRS = '#cc3333';
   var COLOR_GRID = '#cccccc';
   var COLOR_BUTTON_HOVER = '#cccccc';
@@ -188,6 +194,7 @@ playable2 = (function() {
 
     drawStartButton();
     drawSliders();
+    drawSectionBounds();
 
     // If nothing's consumed an event in the queue, then clear it all.
     // um... is this the right thing to do? Could we run into some weird
@@ -300,10 +307,6 @@ playable2 = (function() {
     // text label
     ctx.fillText('% effectiveness of gun locks', xSlider2 + (lineWidth / 2), yTop + lineEdgeHeight + 12);
 
-    // Draw the movable parts
-    ctx.fillStyle = '#cccccc'; // @todo pick better color
-    ctx.textBaseline = 'bottom';
-
     // Slider positions
     function calcSliderPos(xSliderPos, val) {
       return xSliderPos + (lineWidth * (val / 100) - (sliderWidth / 2));
@@ -374,13 +377,183 @@ playable2 = (function() {
       s2Pos = calcSliderPos(xSlider2, vLockEffect);
     }
 
+    ctx.textBaseline = 'bottom';
     // draw Slider 1
+    ctx.fillStyle = '#cccccc'; // @todo pick better color
     ctx.fillRect(s1Pos, yTop, sliderWidth, sliderHeight);
     ctx.fillText(vWithLock + '%', s1Pos + (sliderWidth / 2), yTop - 4);
 
     // draw Slider 2
+    ctx.fillStyle = COLOR_GUN_LOCK;
     ctx.fillRect(s2Pos, yTop, sliderWidth, sliderHeight);
     ctx.fillText(vLockEffect + '%', s2Pos + (sliderWidth / 2), yTop - 4);
+  }
+
+  /**
+   * Draws the bounding areas for all the sections plus updates their
+   * associated labels.
+   */
+  function drawSectionBounds() {
+    var i;
+    var lineWidth = 3;
+    var leftMargin = 12;
+    var marginBwSections = 36;
+    var area1Radius = 90;
+    var area1X = leftMargin + area1Radius;
+    var area1Y = 310;
+
+    ctx.fillStyle = COLOR_BOUNDARIES;
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+
+    // Area 1 - circle. Start persons deci
+    ctx.beginPath();
+    for (i = 0; i < lineWidth; i++) {
+      ctx.ellipse(area1X, area1Y, area1Radius - 1 + i, area1Radius - 1 + i,
+        0 /*rotation*/, 0 /*start angle*/, 2 * Math.PI/*end angle*/);
+    }
+    ctx.stroke();
+    // Label
+    ctx.font = '14px Helvetica';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('Decision to attempt', area1X, area1Y - area1Radius - 20);
+    ctx.fillText('suicide by firearm', area1X, area1Y - area1Radius - 4);
+
+    // Area 2 - Gun lock
+    var area2Width = 132;
+    var area2Height = 72;
+    var area2X = area1X + area1Radius + marginBwSections;
+    var area2Y = 196;
+    // Boundary box
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+    ctx.strokeRect(area2X, area2Y, area2Width, area2Height);
+    ctx.strokeRect(area2X-1, area2Y-1, area2Width+2, area2Height+2);
+    ctx.strokeRect(area2X+1, area2Y+1, area2Width-2, area2Height-2);
+    // Colored side
+    ctx.strokeStyle = COLOR_GUN_LOCK;
+    for (i = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(area2X - 1 + i, area2Y - 2);
+      ctx.lineTo(area2X - 1 + i, area2Y + area2Height + 2);
+      ctx.stroke();
+    }
+    // Label
+    ctx.fillStyle = '#000';
+    ctx.font = '14px Helvetica';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('Gun lock on', area2X + area2Width / 2, area2Y - 18);
+    ctx.fillText('Chance at prevention: ' + vLockEffect + '%', area2X + area2Width / 2, area2Y - 4);
+
+    // Area 3 - Attempt with firearm
+    var area3Width = 132;
+    var area3Height = 72;
+    var area3X = area2X;
+    var area3Y = 344;
+    // Boundary box
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+    ctx.strokeRect(area3X, area3Y, area3Width, area3Height);
+    ctx.strokeRect(area3X-1, area3Y-1, area3Width+2, area3Height+2);
+    ctx.strokeRect(area3X+1, area3Y+1, area3Width-2, area3Height-2);
+    // Colored side
+    ctx.strokeStyle = COLOR_ATTEMPT;
+    for (i = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(area3X - 1 + i, area3Y - 2);
+      ctx.lineTo(area3X - 1 + i, area3Y + area3Height + 2);
+      ctx.stroke();
+    }
+    // Label
+    ctx.fillStyle = '#000';
+    ctx.fillText('Suicide attempt', area3X + area3Width / 2, area3Y - 18);
+    ctx.fillText('Chance of fatality: 85%', area3X + area3Width / 2, area3Y - 4);
+
+    // Area 4 - Saved
+    var area4Width = 156;
+    var area4Height = 70;
+    var area4X = area2X + area2Width + marginBwSections;
+    var area4Y = area2Y;
+    // Boundary box
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+    ctx.strokeRect(area4X, area4Y, area4Width, area4Height);
+    ctx.strokeRect(area4X-1, area4Y-1, area4Width+2, area4Height+2);
+    ctx.strokeRect(area4X+1, area4Y+1, area4Width-2, area4Height-2);
+    // Colored side
+    ctx.strokeStyle = COLOR_SAVED;
+    for (i = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(area4X - 1 + i, area4Y - 2);
+      ctx.lineTo(area4X - 1 + i, area4Y + area4Height + 2);
+      ctx.stroke();
+    }
+    // Label
+    ctx.fillStyle = COLOR_SAVED;
+    ctx.textAlign = 'center';
+    ctx.font = '14px Helvetica';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('XX %', area4X + area4Width + 52, area4Y + (area4Height / 2));
+
+    ctx.fillStyle = '#000';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Saved', area4X + area4Width + 52, area4Y + (area4Height / 2));
+
+    // Area 5 - Unsuccessful
+    var area5Width = 156;
+    var area5Height = 70;
+    var area5X = area4X;
+    var area5Y = area4Y + area4Height + 5;
+    // Boundary box
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+    ctx.strokeRect(area5X, area5Y, area5Width, area5Height);
+    ctx.strokeRect(area5X-1, area5Y-1, area5Width+2, area5Height+2);
+    ctx.strokeRect(area5X+1, area5Y+1, area5Width-2, area5Height-2);
+    // Colored side
+    ctx.strokeStyle = COLOR_UNSUCCESSFUL;
+    for (i = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(area5X - 1 + i, area5Y - 2);
+      ctx.lineTo(area5X - 1 + i, area5Y + area5Height + 2);
+      ctx.stroke();
+    }
+    // Label
+    ctx.fillStyle = COLOR_UNSUCCESSFUL;
+    ctx.textAlign = 'center';
+    ctx.font = '14px Helvetica';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('YY %', area5X + area5Width + 52, area5Y + (area5Height / 2));
+
+    ctx.fillStyle = '#000';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Unsuccessful', area5X + area5Width + 52, area5Y + (area5Height / 2));
+
+    // Area 6 - Fatal
+    var area6Width = 156;
+    var area6Height = 70;
+    var area6X = area4X;
+    var area6Y = area5Y + area5Height + 5;
+    // Boundary box
+    ctx.strokeStyle = COLOR_BOUNDARIES;
+    ctx.strokeRect(area6X, area6Y, area6Width, area6Height);
+    ctx.strokeRect(area6X-1, area6Y-1, area6Width+2, area6Height+2);
+    ctx.strokeRect(area6X+1, area6Y+1, area6Width-2, area6Height-2);
+    // Colored side
+    ctx.strokeStyle = COLOR_FATAL;
+    for (i = 0; i < 3; i++) {
+      ctx.beginPath();
+      ctx.moveTo(area6X - 1 + i, area6Y - 2);
+      ctx.lineTo(area6X - 1 + i, area6Y + area6Height + 2);
+      ctx.stroke();
+    }
+    // Label
+    ctx.fillStyle = COLOR_FATAL;
+    ctx.textAlign = 'center';
+    ctx.font = '14px Helvetica';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('ZZ %', area6X + area6Width + 52, area6Y + (area6Height / 2));
+
+    ctx.fillStyle = '#000';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Fatal', area6X + area6Width + 52, area6Y + (area6Height / 2));
   }
 
   return {
